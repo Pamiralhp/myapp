6aa58b0ee2f5ed11eaebcc9f2b074b872824fc98
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-07-11 19:39:27 +0200

    React

---
+++ src/componentes/Articulo.js
@@ -0,0 +1,19 @@
+import Boton from "./Boton";
+function Articulo() {
+    return (
+    <div className="Articulo">
+    <h3>Contenido</h3>
+    <p>Crea un componente llamado Article con un contenido de un título (h3) y contenido lorem
+ipsum en una etiqueta párrafo, que será utilizado únicamente desde el Componente Main varias
+veces.Crea un componente llamado Article con un contenido de un título (h3) y contenido lorem
+ipsum en una etiqueta párrafo, que será utilizado únicamente desde el Componente Main varias
+veces.Crea un componente llamado Article con un contenido de un título (h3) y contenido lorem
+ipsum en una etiqueta párrafo, que será utilizado únicamente desde el Componente Main varias
+veces.Crea un componente llamado Article con un contenido de un título (h3) y contenido lorem
+ipsum en una etiqueta párrafo, que será utilizado únicamente desde el Componente Main varias
+veces.</p>
+<Boton/>
+    </div>
+    );
+    }
+    export default Articulo;
---
+++ src/componentes/Main.js
@@ -0,0 +1,13 @@
+import Articulo from "./Articulo";
+function Main() {
+    return (
+    <div className="Main">
+    <h3>Contenido principal</h3>
+    <p>Este es el contenido de la applicacion</p>
+    <Articulo/>
+    <Articulo/>
+    <Articulo/>
+    </div>
+    );
+    }
+    export default Main;
